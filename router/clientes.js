74f74e23d5df376085e5086f559b9c26e3ca4f07
andresquimbay0511 <andresquimbay1109@gmail.com>
2021-11-29 11:42:02 -0500

    CRUD_Clientes

--- router/clientes.js
+++ router/clientes.js
@@ -55,4 +55,45 @@ router.get('/:id', async(req, res)=>{
     }
 })
 
-module.exports = router;
+//Borrado de los clientes con CRUD
+router.delete('/:id', async(req, res)=>{
+    const id = req.params.id
+    try{
+        const clienteDB = await Cliente.findByIdAndDelete({_id: id})
+        if(clienteDB) {
+            res.json({
+                estado : true,
+                mensaje : 'Usuario Elimininado !!!'
+            })
+        } else {
+        res.json({
+            estado : false,
+            mensaje : 'El usuario no pudo ser elimininado !!!'
+        })
+    }
+    }catch(error){
+        console.log(error)
+    }
+})
+
+//Crear la ruta para editar los clientes
+router.put('/:id', async(req, res)=>{
+    const id = req.params.id
+    const body = req.body
+    try {
+        const clienteDB = await Cliente.findByIdAndUpdate(
+            id, body, {useFindAndModify: false})
+            console.log(clienteDB)
+            res.json({
+                estado : true,
+                mensaje : 'Cliente Editado'
+            })
+    } catch (error) {
+        res.json({
+            estado : false,
+            mensaje : 'Fallo al Editar'
+        })
+        console.log(error)
+    }
+ })
+module.exports = router;   
